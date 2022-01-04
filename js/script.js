260d055808fd76960faf22ff5aee115672ca52b0
mkwawama <79860095+mkwawama@users.noreply.github.com>
2022-01-04 22:08:26 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+        /*Sound Function
+        AFRAME.registerComponent('play',{
+            init:function() {
+                var myEL=document.querySelector('Radio');
+                this.el.addEventListener('click',function () {
+                    myEL.components.sound.playSound();
+                });
+            }
+        });
+        AFRAME.registerComponent('stop',{
+            init:function() {
+                var myEL=document.querySelector('Radio');
+                this.el.addEventListener('click',function () {
+                    myEL.components.sound.stopSound();
+                });
+            }
+        });*\
+    
